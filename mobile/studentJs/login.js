7aa9757239ef36d6a31ee943ea347d727eb958b4
Huacx <1358407547@qq.com>
2017-04-02 15:51:34 +0800

    1.0.4

--- mobile/studentJs/login.js
+++ mobile/studentJs/login.js
@@ -1,34 +1,89 @@
-$(function() {
-    var login = {
-        init: function() { this.events() },
-        events: function() {
-            $("button").click(function() {
-                var val = $('input:radio[name="status"]:checked').val();
-                if(val == 0){
-	                location.href = "../code/studentIndex.html";
-                }else{
-                	location.href = '../code/teacherIndex.html'
-                }
-            })
+// $(function() {
+//     var flag;
+//     var login = {
+//         init: function() { 
+//             this.events();
+//             this.dealChecked(); 
+//         },
+//         events: function() {
+//             var that = this;
+//             $("#login").click(function() {
+//                 var val = $('input:radio[name="status"]:checked').val();
+//                 that.getInfo();
+//                 if(val == 0){
+// 	                location.href = "../code/studentIndex.html";
+//                 }else{
+//                 	location.href = '../code/teacherIndex.html'
+//                 }
+//             })
+//         },
+//         getInfo:function(){
+//             var username = $('#username').val();
+//             var pwd = $('#password').val();
+//             if($('#rememberMe').is(':checked')){
+//                 flag = true;
+//                 localStorage.username = username;
+//                 localStorage.password = pwd;
+//                 console.log('true');
+//             }
+//             else{
+//                 flag = false;
+//                 console.log('false');
+//             }
+//         },
+//         dealChecked:function(){
+//             if($('#rememberMe').is(':checked')){
+//                 $('#username').val(localStorage.username);
+//                 $('#password').val(localStorage.password);
+//             }
+//         }
+//     };
+//     login.init()
+// });
+var app = angular.module('myapp',[]);
+app.controller('loginController',['$scope','$http',function($scope,$http){
+	// var promise = $http({
+	// 	method:'type',
+	// 	url:'login.js',
+	// 	data:{
+
+	// 	}
+	// });
+	// promise.then(function(res){
+
+	// },function(){
+	// 	console.log('error');
+	// })
+    $scope.state = 0;
+    $scope.remVal = false;
+    $scope.login = function(){
+        if($scope.remVal){
+            localStorage.username = $scope.username;
+            localStorage.password = $scope.password;
         }
-    };
-    login.init()
-});
-// var app = angular.module('myapp',[]);
-// app.controller('loginController',['$scope','$http',function($scope,$http){
-// 	var promise = $http({
-// 		method:'type',
-// 		url:'login.js',
-// 		data:{
-
-// 		}
-// 	});
-// 	promise.then(function(res){
-
-// 	},function(){
-// 		console.log('error');
-// 	})
-// }])
+        if($scope.state == 0){//学生界面
+            location.href = '../code/studentIndex.html'
+        }else{//教师界面
+            location.href = '../code/teacherIndex.html'
+        }
+    }
+    // 记住我选项
+    $scope.remember = function(){
+        $scope.remVal = !$scope.remVal;
+    }
+    // 登录身份选项
+    $scope.radio = function(num){
+        $scope.state = num;
+    }
+    if($scope.remVal){
+        $scope.username = localStorage.username;
+        $scope.password = localStorage.password;
+    }
+    else{
+        $scope.username = '';
+        $scope.password = '';
+    }
+}])
 // $(function() {
 //     var login = {
 //         init: function() {
