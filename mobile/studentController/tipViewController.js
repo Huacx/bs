7c0db24a4dbb2f24ddeecede6d21cbe0269264d5
Huacx <1358407547@qq.com>
2017-05-05 15:57:45 +0800

    'ann'

--- mobile/studentController/tipViewController.js
+++ mobile/studentController/tipViewController.js
@@ -53,24 +53,24 @@ app.controller('tipViewController', ['$scope', '$http', function($scope, $http) 
             $scope.ann_st = 0;
             $scope.ann_content = '作业通知';
             // $scope.ann_date = '2017-05-03';
-            // this.getData();
 
             $scope.showArr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60];
-        	$scope.countAll = $scope.showArr.length;
-        	$scope.ann_date = $scope.showArr.slice(($scope.page-1)*16,($scope.page-1)*16+16);
+            $scope.countAll = $scope.showArr.length;
+            $scope.ann_date = $scope.showArr.slice(($scope.page-1)*16,($scope.page-1)*16+16);
+            this.getData();
             this.events();
         },
         // 获取数据
         getData: function() {
             $http({
-                method: 'post',
+                method: 'get',
                 url: 'http://ourworkmanager.cn/myine/annshow.php',
                 data: {
                     ann_type: $scope.ann_st
                 }
             }).then(function(res) {
                 console.log(res);
-                $scope.showData = res;
+                // $scope.showData = res;
                
             })
         },
--- mobile/teacherJs/add_ann.js
+++ mobile/teacherJs/add_ann.js
@@ -1,29 +1,45 @@
-var app = angular.module('annApp',[]);
-app.controller('annController',['$scope','$http',function($scope,$http){
-	$scope.annTypeNum = 0;
-	$scope.annType = function(num){
-		//0 作业选项
-		if(num == 0){
-			$scope.annTypeNum = num;
-		}
-		//1 考试选项
-		else if(num == 1){
-			$scope.annTypeNum = num;
-		}
-		//2 其他选项
-		else 
-		{
-			$scope.annTypeNum = num;
-		}
-	}
-	$scope.annSubmit = function(){
-		alert($scope.annTypeNum + $scope.annTitle + $scope.annContent);
-		// location.href='../code/teacherIndex.html#!/homeWork';
-		location.href='../code/teacherIndex.html#!/homeWork';
-		// window.close();
-	}
-	$scope.annCancel = function(){
-		location.href='../code/teacherIndex.html#!/homeWork';
-		// window.close();
-	}
+var app = angular.module('annApp', []);
+app.controller('annController', ['$scope', '$http', function($scope, $http) {
+    $scope.annTypeNum = 0;
+    $scope.annType = function(num) {
+        //0 作业选项
+        if (num == 0) {
+            $scope.annTypeNum = num;
+        }
+        //1 考试选项
+        else if (num == 1) {
+            $scope.annTypeNum = num;
+        }
+        //2 其他选项
+        else {
+            $scope.annTypeNum = num;
+        }
+    }
+    $scope.annSubmit = function() {
+        alert($scope.annTypeNum + $scope.annTitle + $scope.annContent);
+        // location.href='../code/teacherIndex.html#!/homeWork';
+        location.href = '../code/teacherIndex.html#!/homeWork';
+        // window.close();
+    }
+    $scope.annCancel = function() {
+        location.href = '../code/teacherIndex.html#!/homeWork';
+        // window.close();
+    }
+    $scope.annSubmit = function() {
+        $http({
+            url: 'http://ourworkmanager.cn/myine/announce.php',
+            method: 'get',
+            data: {
+                ann_type: $scope.annType,
+                ann_title: $scope.annTitle,
+                ann_content: $scope.annContent
+            }
+        }).then(function(res) {
+            console.log(res);
+        })
+    }
+    $scope.annCancel = function() {
+        location.href = '../code/teacherIndex.html#!/homeWork'
+    }
+
 }])
